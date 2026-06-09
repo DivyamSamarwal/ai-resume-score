6b628ea71d0094981318d57b68cd6fc6c3ae4804
Divyam Samarwal <divyamsamarwal@gmail.com>
2026-06-09 11:41:30 +0530

    Update Dashboard.tsx

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import React, { useState } from 'react';
 import type { EvaluationResult, PillarScore } from '@/types';
 import ScoreRing from './ScoreRing';
 import PillarBar from './PillarBar';
